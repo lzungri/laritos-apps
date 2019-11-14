45b287821628cd80652172fc9637d6ee96c5958e
Leandro Zungri <lzungri@gmail.com>
2019-11-14 15:51:56 -0800

    hello: Simplify hello world app

--- hello-world/hello.c
+++ hello-world/hello.c
@@ -1,28 +1,6 @@
 #include <stdio.h>
 
-char data[0xaa];
-char datac[16] = { 1, 1, 1 };
-char datac2[16] = { 2, 2, 2 };
-char datac3[16] = { 3, 3, 3 };
-
-void func(void) {
-    data[2] = 5;
-    datac3[2] = 4;
-
-    asm("svc 1");
-}
-
-void func2(void) {
-    data[1] = 5;
-    func();
-    datac3[1] = 5;
-}
-
 int main(void) {
 //    puts("Hello World!");
-    func2();
-    data[0] = 4;
-    datac[0] = 4;
-    datac2[0] = 4;
     return 0;
 }
