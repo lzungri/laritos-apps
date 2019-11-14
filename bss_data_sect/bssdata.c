9c5cf91766fce52019a24c71ea7d564a439cf33d
Leandro Zungri <lzungri@gmail.com>
2019-11-14 15:51:39 -0800

    bssdata: Add app for testing bss and data sections

---
+++ bss_data_sect/bssdata.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+char data[0xaa];
+char datac[16] = { 1, 1, 1 };
+char datac2[16] = { 2, 2, 2 };
+char datac3[16] = { 3, 3, 3 };
+
+void func(void) {
+    data[2] = 5;
+    datac3[2] = 4;
+}
+
+void func2(void) {
+    data[1] = 5;
+    func();
+    datac3[1] = 5;
+}
+
+int main(void) {
+    func2();
+    data[0] = 4;
+    datac[0] = 4;
+    datac2[0] = 4;
+    return 0;
+}
