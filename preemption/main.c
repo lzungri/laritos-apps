6b6338e6a9ece37e711b38a499e901c9fafc1cba
Leandro Zungri <lzungri@gmail.com>
2019-12-02 01:47:55 -0800

    preemption: Print pid on every second

--- preemption/main.c
+++ preemption/main.c
@@ -1,6 +1,18 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <printf.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void) {
-    while (1);
+    time_t cur, prev;
+    time(&prev);
+    while (1) {
+        time(&cur);
+        if (cur.secs != prev.secs) {
+            printf("pid=%u, time=%lu", getpid(), (uint32_t) cur.secs);
+            prev = cur;
+        }
+    }
     return 0;
 }
