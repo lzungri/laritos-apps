5f72c4fa91b9b7a3abbe785e98dfd3c0c6d3cd85
Leandro Zungri <lzungri@gmail.com>
2019-12-06 19:01:18 -0800

    procname: Add app to test set_process_name() syscall

---
+++ procname/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <printf.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main(void) {
+    switch (getpid() % 2) {
+    case 0:
+        set_process_name("Proc even");
+        break;
+    case 1:
+        set_process_name("Proc odd");
+        break;
+    default:
+        break;
+    }
+
+    while (1);
+
+    return 0;
+}
