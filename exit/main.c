816a37a15f04777afe500edaf798b90f97acb099
Leandro Zungri <lzungri@gmail.com>
2020-02-19 13:15:37 -0800

    app: Add new line char to each puts() call

--- exit/main.c
+++ exit/main.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 static void f1(void) {
-    puts("f1 begin");
+    puts("f1 begin\n");
     exit(1);
-    puts("f1 end (should not be printed)");
+    puts("f1 end (should not be printed)\n");
 }
 
 int main(void) {
-    puts("Starting 'exit' process");
+    puts("Starting 'exit' process\n");
     f1();
-    puts("This message should not be printed");
+    puts("This message should not be printed\n");
     return 0;
 }
