455eedf3cde550fbcfa6ce1724bc4162fcba6728
Leandro Zungri <lzungri@gmail.com>
2020-03-16 14:21:33 -0700

    sleep: Remove busy wait loop

--- sleep/main.c
+++ sleep/main.c
@@ -10,11 +10,6 @@ int main(void) {
 
     unsigned char i;
     for (i = 3; i < 6; i++) {
-        time_t t;
-        time(&t);
-        uint64_t deadline = t.secs + 6;
-        while (time(&t) >= 0 && t.secs < deadline);
-
         printf("Sleeping for %u seconds\n", i);
         sleep(i);
         printf("Waking up\n");
